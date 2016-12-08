5af7ce83be1d90572d4a922f72b142b4d4991de8
W.B. Metzel <w.metzel@gmail.com>
2016-12-08 11:58:42 -0500

    Implemented majority of print

--- Menu.cpp
+++ Menu.cpp
@@ -11,7 +11,7 @@
 ********************************/
 Menu::Menu()
 {
-  this->items = 0;
+  //this->options;
   this->title = "Title";
 }
 
@@ -25,8 +25,9 @@ Menu::Menu()
 ********************************/
 Menu::Menu(const string &title)
 {
-  this->items = 0;
+  //this->options;
   this->title = title;
+  this->width = this->setWidth();
 }
 
 
@@ -39,6 +40,28 @@ Menu::Menu(const string &title)
 ********************************/
 Menu::~Menu()
 {}
+
+
+/*******************************
+*
+* f(x): setWidth
+* Created: 07 Dec 2016
+* Purpose: Set width of menu
+*    If length of title is less than 48, width minimum is 50
+*    No max is set, since I only intend to name titles and use this class
+********************************/
+int Menu::setWidth()
+{
+  if( (this->title).size() <= 46 )
+  {
+    return 50;
+  }
+  else
+  {
+    return (this->title).size() + 4;
+  }
+
+}
 
 
 /*******************************
@@ -60,14 +83,102 @@ void Menu::clear()
 
 /*******************************
 *
+* f(x): insert
+* Created: 08 Dec 2016
+* Purpose: Add item to options
+*
+********************************/
+void Menu::insert()
+{
+}
+
+
+/*******************************
+*
+* f(x): indent
+* Created: 08 Dec 2016
+* Purpose: print 5 blank spaces
+*
+********************************/
+void Menu::indent(const int &indentSize)
+{
+  std::cout.fill(' ') << std::cout.width(indentSize);  // Indent 
+}
+
+
+/*******************************
+*
 * f(x): print
-* Created: 07 Dec 2016
+* Created: 08 Dec 2016
 * Purpose: print menu
 *
 ********************************/
 void Menu::print()
 {
-  this->clear();
+    // where to start printing title to center, NOT center of menu width
+  unsigned short int centerFill = (this->width/2) - ((this->title).size() / 2);
+  unsigned short int indentSize = 10;
+
+  this->clear();  // Clear screen
   
-  std::cout << "\t\t" << this->title << "\n" << std::endl;
+    // Print top border
+  this->indent(indentSize);
+  std::cout << char(201);
+  std::cout.fill(205) << std::cout.width(width-2);
+  std::cout << char(187) << std::endl;
+  
+    // Blank fill line
+  this->indent(indentSize);
+  std::cout << char(186);
+  std::cout.fill(32) << std::cout.width(width-2);
+  std::cout << char(186) << std::endl;
+  
+    // Print title, centered horizontally
+  this->indent(indentSize);
+  std::cout << char(186); // We are now indentSize + 1 chars into line.
+  std::cout.fill(32) << std::cout.width(width - centerFill - 1);
+  std::cout << this->title;
+  std::cout.fill(32) << std::cout.width(centerFill - 1);
+  std::cout << char(186) << std::endl;
+  
+    // Blank fill line
+  this->indent(indentSize);
+  std::cout << char(186);
+  std::cout.fill(32) << std::cout.width(width-2);
+  std::cout << char(186) << std::endl;
+  
+      // Print middle border
+  this->indent(indentSize);
+  std::cout << char(204);
+  std::cout.fill(205) << std::cout.width(width-2);
+  std::cout << char(185) << std::endl;
+  
+    // Blank fill line
+  this->indent(indentSize);
+  std::cout << char(186);
+  std::cout.fill(32) << std::cout.width(width-2);
+  std::cout << char(186) << std::endl;
+  
+  // Loop through list items
+  
+      // Print menu options, options descriptions must be shorter than 46 chars!!
+    this->indent(indentSize);
+    std::cout << char(186);
+    std::cout.fill(32) << std::cout.width(width - centerFill - 1);
+    std::cout << this->title;
+    std::cout.fill(32) << std::cout.width(centerFill - 1);
+    std::cout << char(186) << std::endl;
+  
+    // Blank fill line
+  this->indent(indentSize);
+  std::cout << char(186);
+  std::cout.fill(32) << std::cout.width(width-2);
+  std::cout << char(186) << std::endl;
+  
+    // Print bottom border
+  this->indent(indentSize);
+  std::cout << char(200);
+  std::cout.fill(205) << std::cout.width(width-2);
+  std::cout << char(188) << std::endl;
+  
 }
--- Menu.h
+++ Menu.h
@@ -9,23 +9,31 @@
 #ifndef __MENU_H__
 #define __MENU_H__
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using std::string;
+
+const short int ITEMS_MAX = 10;
 
 class Menu
 {
 private:
-  int items;
+  string options[ITEMS_MAX];
   string title;
+  unsigned short int width;
   
+  int setWidth();
+  void indent(const int &indentSize);
   void clear();
 public:
   Menu();
   Menu(const string &title);
   virtual ~Menu();
+  void insert(const string &item);
   void print();
+  // Select returns item
   
 };
 
